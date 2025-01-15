b944543bef83771f0e80adcfe294399243ea677f
Mohamed Ainanshe <mohamed0958@outlook.com>
2025-01-15 19:28:43 +0000

    Complete task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...param) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!param.includes(array[i])) {
+            result.push(array[i]);
+        }
+       
+    }
+
+    return result;
+};  
 
 // Do not edit below this line
 module.exports = removeFromArray;

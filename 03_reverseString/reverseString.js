47eae7fba1c5499807b87de88b6c525143fc3fcf
Mohamed Ainanshe <mohamed0958@outlook.com>
2025-01-15 19:16:45 +0000

    Complete task 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    /**
+     * Reverse a string
+     * Start an array looping backwards and add each index
+     * to a string
+     */
+    let result = "";
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        result = result + string.charAt(i);
+    }
+
+    return result;
 };
 
 // Do not edit below this line
